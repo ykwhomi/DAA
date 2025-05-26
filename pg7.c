8d316a47cf07dfe0f838daa684b8d824bb40e635
ykwhomi <3rvision12@gmail.com>
2025-05-26 07:18:44 +0530

    Update pg7.c

--- pg7.c
+++ pg7.c
@@ -1,57 +1,61 @@
 #include <stdio.h>
-int max(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
-void main()
-{
-    int n, j, i, capacity;
-    int weight[20], value[20];
-    int v[50][50], w;
-    printf("\n\n\t\tEnter the number of items:");
+
+int main() {
+    int n, capacity, i, j, w;
+    int weight[20], value[20], included[20] = {0};
+    int v[50][50];
+
+    printf("Enter the no of items: ");
     scanf("%d", &n);
-    printf("\t\t--------------------------------\n");
-    printf("\n\t\t Enter : WEIGHTS -VALUES \n");
-    printf("\t\t--------------------------------\n");
-    printf("\t\t--------------------------------\n\t\t");
-    for (i = 1; i <= n; i++)
-    {
+
+    printf("Enter the wt of %d item: ", n);
+    for (i = 1; i <= n; i++) {
         scanf("%d", &weight[i]);
-        scanf("%d", &value[i]);
-        printf("\t\t");
     }
-    printf("--------------------------------\n");
-    printf("\t\tEnter the capacity of KnapSack:");
+
+    printf("Enter the values (profit): ");
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &value[i]);
+    }
+
+    printf("Enter the knapsack capacity: ");
     scanf("%d", &capacity);
-    printf("\t\t--------------------------------\n\t\t");
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= capacity; j++)
-        {
+
+    // Fill DP table
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= capacity; j++) {
             if (i == 0 || j == 0)
                 v[i][j] = 0;
-                else if (j - weight[i] >= 0)
-                v[i][j] = max(v[i - 1][j], v[i - 1][j - weight[i]] + value[i]); 
-            v[i][j] = v[i - 1][j];
-            printf("%4d", v[i][j]);
+            else if (weight[i] <= j)
+                v[i][j] = max(v[i - 1][j], v[i - 1][j - weight[i]] + value[i]);
+            else
+                v[i][j] = v[i - 1][j];
+
+            // Print table value immediately
+            printf("%d\t", v[i][j]);
         }
-        printf("\n\t\t");
+        printf("\n");
     }
+
+    // Backtrack to find selected items
     w = capacity;
-    printf("The items in the knapsack :\n");
-    for (i = n; i > 0; i--)
-    {
-        printf("\t\t");
-        if (v[i][w] == v[i - 1][w])
-            continue;
-        else
-        {
-            w = w - weight[i];
-            printf("%3d", i);
+    for (i = n; i > 0; i--) {
+        if (v[i][w] != v[i - 1][w]) {
+            included[i] = 1;
+            w -= weight[i];
         }
     }
-    printf("\n\t\t Total Profit=%d", v[n][capacity]);
+
+    printf("The o.s is %d\n", v[n][capacity]);
+    printf("The validity of items are:\n");
+    printf("Item: Validity: Weight: Profit:\n");
+    for (i = 1; i <= n; i++) {
+        printf("[%d]:   %d     :   %d   :  %d :\n", i, included[i], weight[i], value[i]);
+    }
+
+    return 0;
 }
